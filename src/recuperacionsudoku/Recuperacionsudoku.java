5a1c8c1271ee1b50c8ec1fc66973d953537bb384
tunmi <tunmi@tunmi.home>
2018-03-31 21:18:59 +0200

    1-el scaner 2- hacemos el menu para ver las opciones que queramos del juego 3- los if para coger el menu y que salga lo q elegimos

--- src/recuperacionsudoku/Recuperacionsudoku.java
+++ src/recuperacionsudoku/Recuperacionsudoku.java
@@ -5,6 +5,8 @@
  */
 package recuperacionsudoku;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tunmi
@@ -16,6 +18,41 @@ public class Recuperacionsudoku {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //teclado para la salidad por pantalla
+        Scanner teclado = new Scanner (System.in);
+        int opcion = 0;
+        
+        //este es un menu que tiene opcciones del juego
+        System.out.println("-------Menu sudoku------------------");
+        System.out.println("1---- iniciar o reiniciar sudoku");
+        System.out.println("2---- realizar movimiento");
+        System.out.println("3---- vaciar casilla");
+        System.out.println("4---- mostrar sudoku");
+        System.out.println("5---- terminar partida");
+        System.out.println("-------------------------------------");
+        
+        
+        System.out.println("Dime que opcion quieres elegir:");
+        opcion = teclado.nextInt();
+        
+        //los if para ver que opcion se coge
+        if(opcion == 1){
+            System.out.println("has elegido la "+opcion);
+        }
+        if(opcion == 2){
+            System.out.println("has elegido la "+opcion);
+        }
+        if(opcion == 3){
+            System.out.println("has elegido la "+opcion);
+        }
+        if(opcion == 4){
+            System.out.println("has elegido la "+opcion);
+        }
+        if(opcion == 5){
+            System.out.println("has elegido la "+opcion);
+        
+    }
+    
     }
     
 }
