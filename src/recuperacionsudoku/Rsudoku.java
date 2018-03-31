0a7f5b4f80a6fb3af8f07366d03fb1a3ec68ae74
tunmi <tunmi@tunmi.home>
2018-03-31 21:26:08 +0200

    1-creamos la clase 2-hacemos el constructor y el metodo toString

---
+++ src/recuperacionsudoku/Rsudoku.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recuperacionsudoku;
+
+/**
+ *
+ * @author tunmi
+ */
+public class Rsudoku {
+    
+        public Rsudoku()
+      {
+      }
+      
+        public void inicializar()
+      {
+      }
+      
+      
+      @Override
+      public String toString()
+      {
+      String resultadoFinal = "";
+      return resultadoFinal;
+      }
+}
+      
+      
